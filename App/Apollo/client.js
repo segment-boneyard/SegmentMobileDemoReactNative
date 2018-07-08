07ef988274c642a66120afbdbfb8b7c4e64cae34
ikrtolica <igor@blinkinc.com>
2018-07-08 13:03:57 -0700

    WIP - added correct products query

--- App/Apollo/client.js
+++ App/Apollo/client.js
@@ -12,27 +12,79 @@ const AUTH_TOKEN = 'd5e4776a79398e8b1160b1dad613e17a';
 const networkInterface = { uri: NETWORK_INTERFACE_URL };
 const httpLink = createHttpLink(networkInterface);
 
-const authLink = setContext(() => ({
-  headers: {
-    'X-Shopify-Storefront-Access-Token': `${AUTH_TOKEN}`,
-  }
-}));
+const authLink = setContext(() => {
+  return {
+    headers: {
+      'X-Shopify-Storefront-Access-Token': `${AUTH_TOKEN}`,
+    }
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
+// Fetches all products and variants.  This is from the Shopify
+// example React app:  https://github.com/Shopify/storefront-api-examples/blob/master/react-apollo/src/App.js
+
 export const fetchProducts = () => {
   return client.query({
     query: gql`
-      query {
-        shop {
-          name
-          description
+    query query {
+      shop {
+      name
+      description
+      products(first:20) {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
+        edges {
+          node {
+            id
+            title
+            options {
+              id
+              name
+              values
+            }
+            variants(first: 250) {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+              }
+              edges {
+                node {
+                  id
+                  title
+                  selectedOptions {
+                    name
+                    value
+                  }
+                  image {
+                    src
+                  }
+                  price
+                }
+              }
+            }
+            images(first: 250) {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+              }
+              edges {
+                node {
+                  src
+                }
+              }
+            }
+          }
         }
       }
-    `
+    }
+  }`
   });
 };
 
