046268fc78567fceba5c2be0da73d5922b5bbb33
Igor <igor.fcb@gmail.com>
2018-10-10 14:37:01 -0700

    Fix text string issue
    From here: https://github.com/facebook/react-native/issues/20084

--- App/Containers/ProductDetailScreen.js
+++ App/Containers/ProductDetailScreen.js
@@ -51,51 +51,17 @@ export class ProductDetailScreen extends Component {
     const { params } = this.props.navigation.state;
     return (
       <View style={{ flex: 1, backgroundColor: "rgba(255,255,255, 0.75)" }}>
-        // Header
-        <NavigationHeader
-          title={"PRODUCT"}
-          navigation={this.props.navigation}
-        />
-        // Content
+        <NavigationHeader title={"PRODUCT"} navigation={this.props.navigation} />
         <View style={{ flex: 1, marginTop: 10 }}>
-          <View
-            style={{
-              flex: 0.1,
-              flexDirection: "row",
-              margin: 5,
-              justifyContent: "space-between"
-            }}
-          >
+          <View style={{ flex: 0.1, flexDirection: "row", margin: 5, justifyContent: "space-between" }} >
             <Text style={styles.itemLabel}>{params.title}</Text>
-            <Text style={styles.itemPrice}>
-              {`$${params.variants.edges[0].node.price}`}
-            </Text>
+            <Text style={styles.itemPrice}>{`$${params.variants.edges[0].node.price}`}</Text>
           </View>
-          <View
-            style={{
-              flex: 0.9,
-              flexDirection: "column",
-              marginLeft: 5,
-              marginRight: 5
-            }}
-          >
-            <Image
-              style={styles.imageStyle}
-              source={{ uri: `${params.variants.edges[0].node.image.src}` }}
-            />
-            <Text style={{ marginTop: 20, fontSize: 18, margin: 2 }}>
-              {params.description}
-            </Text>
-            <VariantSelector
-              variants={params.variants}
-              selectedVariant={this.props.selectedVariant}
-              handleOptionChange={this.handleVariantChange}
-            />
-            <FullButton
-              style={{ margin: 5 }}
-              onPress={this.handleAddToCart}
-              text={"ADD TO CART"}
-            />
+          <View style={{ flex: 0.9, flexDirection: "column", marginLeft: 5, marginRight: 5 }} >
+            <Image style={styles.imageStyle} source={{ uri: `${params.variants.edges[0].node.image.src}` }} />
+            <Text style={{ marginTop: 20, fontSize: 18, margin: 2 }}>{params.description}</Text>
+            <VariantSelector variants={params.variants} selectedVariant={this.props.selectedVariant} handleOptionChange={this.handleVariantChange} />
+            <FullButton style={{ margin: 5 }} onPress={this.handleAddToCart} text={"ADD TO CART"} />
           </View>
         </View>
       </View>
